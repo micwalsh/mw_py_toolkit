b019adf4728cebfc292128a0fbbd43d0a4ac768e
Michael Walsh <micwalsh@us.ibm.com>
2020-02-13 14:46:03 -0600

    New __init__.pyc

---
+++ __init__.py
@@ -0,0 +1,11 @@
+import sys
+import os
+import argparse
+
+# Add the mwtoolkit/lib subdirectory to sys.path to allow modules to import sister modules.
+# Note: All directory path values will include a trailing slash.
+package_base_dir_path = os.path.dirname(os.path.realpath(__file__)) + os.sep
+sys.path.append(package_base_dir_path + "lib")
+
+from .lib import gen_arg as ga, gen_print as gp, gen_valid as gv, gen_cmd as gc, gen_misc as gm, \
+    var_funcs as vf, func_timer as ft, func_args as fa, var_stack as vs
